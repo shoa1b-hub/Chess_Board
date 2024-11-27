4af5f7594a1e23869256162cfbec317a6ff357bd
Shoaib <69454473+shoa1b-hub@users.noreply.github.com>
2024-11-27 15:04:59 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Chessboard from './components/Chessboard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Chessboard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
